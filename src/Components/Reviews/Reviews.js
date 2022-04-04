ab1a4292154db021a7fedac8e5c36d4d5a7c6623
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-04-04 18:45:29 +0600

    final commit

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -5,10 +5,14 @@ import ReviewPage from '../ReviewPage/ReviewPage';
 const Reviews = () => {
     const [movies,setMovies] = useMovies();
     return (
-        <div className=' grid md:grid-cols-3 gap-10 items-center my-8 mx-40'>
+        
+        <div>
+            <h1 className='text-center text-5xl text-emerald-700'>What our Audience says </h1>
+            <div className=' grid md:grid-cols-3 gap-10 items-center my-8 mx-40'>
             {
                 movies.map(movie=><ReviewPage key={movie.id} movie={movie}></ReviewPage>)
             }
+        </div>
         </div>
     );
 };

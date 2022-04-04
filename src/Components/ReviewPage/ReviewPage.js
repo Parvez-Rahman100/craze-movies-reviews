372821efdb93c1e8def7685adf4bef1655c893c4
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-04-04 18:55:37 +0600

    review updated

--- src/Components/ReviewPage/ReviewPage.js
+++ src/Components/ReviewPage/ReviewPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ReviewPage = ({movie}) => {
-    const {name,img,Rating,review,reviewer} = movie;
+    const {name,img,Rating,review,reviewer,reviewerName} = movie;
     return (
         <div className=' border-slate-800 border p-10 rounded-xl bg-teal-50'>
              <div className=' w-full' >
@@ -10,6 +10,7 @@ const ReviewPage = ({movie}) => {
             <div className='my-3'>
                 <h1 className='text-2xl'>{name}</h1>
                 <p className='text-3xl'>Rating : {Rating}</p>
+                <p className='my-2'>Reviewer : {reviewerName}</p>
             </div>
             <div className='flex justify-center items-center my-3'>
             <img className=' w-10 h-10 rounded-full mr-3' src={reviewer} alt="" />
--- src/MovieReview/MovieReview.js
+++ src/MovieReview/MovieReview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MovieReview = ({movie}) => {
-    const {img,name,Rating,review,reviewer} = movie;
+    const {img,name,Rating,review,reviewer,reviewerName} = movie;
     return (
         <div className=' border-slate-800 border p-10 rounded-xl bg-teal-50'>
         <div className=' w-full' >
@@ -10,6 +10,7 @@ const MovieReview = ({movie}) => {
        <div className='my-3'>
            <h1 className='text-2xl'>{name}</h1>
            <p className='text-3xl'>Rating : {Rating}</p>
+           <p className='my-2'>Reviewer : {reviewerName}</p>
        </div>
        <div className='flex justify-center items-center my-3'>
        <img className=' w-10 h-10 rounded-full mr-3' src={reviewer} alt="" />
